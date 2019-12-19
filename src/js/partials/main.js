ba5ea15b644bd3f9d1cd2f2606ff6ef9e2bb0ffa
PipZip <37911683+PipZip@users.noreply.github.com>
2019-12-19 22:38:29 +0600

    temp

--- src/js/partials/main.js
+++ src/js/partials/main.js
@@ -2,5 +2,20 @@ $(document).ready(function() {
     $("input[name='phone']").mask(" +7 (999) 999-99-99");
 
 
+    $('.photos-slider').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3
+    });
+
+    $('.js-form-show').click(function () {
+        $('.take-part').slideToggle({
+            start: function () {
+                $(this).css({
+                    display: "flex"
+                })
+            }
+        });
+    });
 
 });
