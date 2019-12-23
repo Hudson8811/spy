c754646ac4a0086184814c55778744c8c73342c5
PipZip <37911683+PipZip@users.noreply.github.com>
2019-12-23 21:33:59 +0600

    temp

--- build/js/main.js
+++ build/js/main.js
@@ -33,7 +33,17 @@ $(document).ready(function () {
     $('.photos-slider').slick({
         infinite: true,
         slidesToShow: 3,
-        slidesToScroll: 3
+        slidesToScroll: 3,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    adaptiveHeight: true
+                }
+            }
+        ]
     });
 
     $('.js-form-show').click(function () {
